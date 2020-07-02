b4c679b1d81317b65bc02c9cafb027fdf91b302a
ELASHMAWY DEV <contact@elashmawydev.com>
2020-07-02 12:22:17 +0200

    config and log files excluded

--- includes/config.php
+++ includes/config.php
@@ -12,9 +12,9 @@ ini_set('errors_log', 'errors.log');
 @
 */
 $db_host  = 'localhost';
-$db_user  = 'zawghivc_zwaj';
-$db_pass  = 'zawghivc_zwaj';
-$db_name  = 'zawghiv1234';
+$db_user  = 'root';
+$db_pass  = '';
+$db_name  = 'zwaj';
 #
 #mysqli class
 #
